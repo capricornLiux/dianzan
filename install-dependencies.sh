61719add0d0d75b14fe62371b00df7dff2618746
liuxin <784532927@qq.com>
2018-03-10 11:48:02 +0800

    修改sudo权限

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,1 @@
-npm install --save-dev babel-cli babel-preset-env babel-preset-es2015 chai jasmine-core karma karma-chrome-launcher karma-cli karma-jasmine karma-phantomjs-launcher mocha phantomjs
+sudo npm install --save-dev babel-cli babel-preset-env babel-preset-es2015 chai jasmine-core karma karma-chrome-launcher karma-cli karma-jasmine karma-phantomjs-launcher mocha phantomjs
